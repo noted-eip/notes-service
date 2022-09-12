6042e397e0ac2d951b026a83be6ed1d647528657
Gabriel <gabriel.medoukali@epitech.eu>
2022-09-12 22:44:27 +0900

    fix: temporary commit for saving

--- misc/gen_proto.sh
+++ misc/gen_proto.sh
@@ -1,2 +1,2 @@
-rm -rf protorepo/noted/notes/v1/*pb/
-protoc --go_out=. --go-grpc_out=. protorepo/noted/notes/v1/*.proto
+rm -rf grpc/*pb/
+protoc --go_out=. --go-grpc_out=. grpc/protos/notes/*.proto
