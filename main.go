7e0f25d688585671bda063a759243f277ab85791
Gabriel <gabriel.medoukali@epitech.eu>
2022-12-06 11:42:40 +0900

    fix: removed recommendationUrl useless in this branch

--- main.go
+++ main.go
@@ -13,12 +13,11 @@ import (
 var (
 	app = kingpin.New("notes-service", "Notes service for the Noted backend").DefaultEnvars()
 
-	environment       = app.Flag("env", "either development or production").Default(envIsProd).Enum(envIsProd, envIsDev)
-	port              = app.Flag("port", "grpc server port").Default("3000").Int()
-	mongoUri          = app.Flag("mongo-uri", "mongo uri with password to connect client").Default("mongodb://localhost:27017").String()
-	mongoDbName       = app.Flag("mongo-db-name", "name of the mongo database").Default("notes-service").String()
-	jwtPrivateKey     = app.Flag("jwt-private-key", "base64 encoded ed25519 private key").Default("SGfCQAb05CtmhEesWxcrfXSQR6JjmEMeyjR7Mo21S60ZDW9VVTUuCvEMlGjlqiw4I/z8T11KqAXexvGIPiuffA==").String()
-	recommandationUrl = app.Flag("recommandationUrl", "recommandation api Url").Default("https://localhost:3000").String()
+	environment   = app.Flag("env", "either development or production").Default(envIsProd).Enum(envIsProd, envIsDev)
+	port          = app.Flag("port", "grpc server port").Default("3000").Int()
+	mongoUri      = app.Flag("mongo-uri", "mongo uri with password to connect client").Default("mongodb://localhost:27017").String()
+	mongoDbName   = app.Flag("mongo-db-name", "name of the mongo database").Default("notes-service").String()
+	jwtPrivateKey = app.Flag("jwt-private-key", "base64 encoded ed25519 private key").Default("SGfCQAb05CtmhEesWxcrfXSQR6JjmEMeyjR7Mo21S60ZDW9VVTUuCvEMlGjlqiw4I/z8T11KqAXexvGIPiuffA==").String()
 )
 
 var (
