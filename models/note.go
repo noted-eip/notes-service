fb6c3ebd6eb3d3e4244e3d0fc4f83ec84fa7ba4a
Edouard SENGEISSEN <edouard.sengeissen@epitech.eu>
2023-01-03 16:47:21 +0900

    feat: Keyword model and Note model updated

--- models/note.go
+++ models/note.go
@@ -7,11 +7,37 @@ import (
 	"time"
 )
 
+type KeywordType string
+
+// Note: Enum just to be sure that there won't be any other strings used in the `Type` field, dk if it's useful
+const (
+	Unknown      KeywordType = "Unknown"
+	Person       KeywordType = "Person"
+	Location     KeywordType = "Location"
+	Organization KeywordType = "Organization"
+	Event        KeywordType = "Event"
+	WorkOfArt    KeywordType = "Work of art"
+	ConsumerGood KeywordType = "Consumer good"
+	Other        KeywordType = "Other"
+	PhoneNumber  KeywordType = "Phone number"
+	Address      KeywordType = "Address"
+	Date         KeywordType = "Date"
+	Number       KeywordType = "Number"
+	Price        KeywordType = "Price"
+)
+
+type Keyword struct {
+	Keyword string      `json:"keyword" bson:"keyword,omitempty"`
+	Type    KeywordType `json:"type" bson:"type,omitempty"`
+	URL     string      `json:"url" bson:"url,omitempty"`
+}
+
 type Note struct {
 	ID               string    `json:"id" bson:"_id,omitempty"`
 	AuthorId         string    `json:"authorId" bson:"authorId,omitempty"`
 	Title            string    `json:"title" bson:"title,omitempty"`
 	Blocks           []Block   `json:"blocks" bson:"blocks,omitempty"`
+	Keywords         []Keyword `json:"keywords" bson:"keywords,omitempty"`
 	CreationDate     time.Time `json:"creationDate" bson:"creationDate,omitempty"`
 	ModificationDate time.Time `json:"modificationDate" bson:"modificationDate,omitempty"`
 }
