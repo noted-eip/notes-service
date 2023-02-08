54f3d2aa2b4fa208b18368e243447fa5cf46cdb7
Gabriel <gabriel.medoukali@epitech.eu>
2023-02-08 19:14:04 +0900

    feat: generateTags

--- language.go
+++ language.go
@@ -1,19 +1,44 @@
 package main
 
-// func (srv *notesService) generateNoteTagsToModelNote(note *models.Note) error {
-// 	var fullNote string
+import (
+	"notes-service/models"
 
-// 	// for _, block := range note.Blocks {
-// 	// 	if block.Type != uint32(notespb.Block_TYPE_CODE) && block.Type != uint32(notespb.Block_TYPE_IMAGE) {
-// 	// 		fullNote += block.Content + "\n"
-// 	// 	}
-// 	// }
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+)
 
-// 	keywords, err := srv.language.GetKeywordsFromTextInput(fullNote)
-// 	if err != nil {
-// 		return status.Error(codes.Internal, err.Error())
-// 	}
+func (srv *notesAPI) generateNoteTagsToModelNote(note *models.Note) error {
+	var fullNote string
 
-// 	note.Keywords = keywords
-// 	return nil
-// }
+	for _, block := range note.Blocks {
+		content, ok := getContent(&block)
+		if ok {
+			fullNote += content + "\n"
+		}
+	}
+
+	keywords, err := srv.language.GetKeywordsFromTextInput(fullNote)
+	if err != nil {
+		return status.Error(codes.Internal, err.Error())
+	}
+
+	note.Keywords = keywords
+	return nil
+}
+
+func getContent(block *models.NoteBlock) (string, bool) {
+	switch block.Type {
+	case "heading":
+		return *block.Heading, true
+	case "paragraph":
+		return *block.Paragraph, true
+	case "math":
+		return *block.Math, true
+	case "bulletpoint":
+		return *block.BulletPoint, true
+	case "numberpoint":
+		return *block.NumberPoint, true
+	default:
+		return "", false
+	}
+}
